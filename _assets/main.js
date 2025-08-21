7ec0c16398cda0d089d69da1ba7a18e0756dc9fc
Giuslock <dev.ablaze777@passinbox.com>
2025-08-21 13:18:05 +0200

    REGULAR COMMIT BY JOPLIN PAGES PUBLISHER(v1.7.1)

--- _assets/main.js
+++
@@ -1,8 +0,0 @@
-// Placeholder for future enhancements (copy-to-clipboard, code line highlight, etc.)
-document.addEventListener('DOMContentLoaded', () => {
-  // External links
-  document.querySelectorAll('a[href^="http"]').forEach(a => {
-    a.setAttribute('target','_blank');
-    a.setAttribute('rel','noopener');
-  });
-});
